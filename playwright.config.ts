bce2e9ea89ea57bf4790ac9d5a0544c8115cc051
ishita mishra <ishitamishra@ishitas-MacBook-Air.local>
2026-07-30 18:52:37 +0530

    Add package lock

---
+++ playwright.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testMatch: 'tests/alert.test.ts',
+  use: {
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  },
+  retries: 0,
+  reporter: [['dot'],['json', {outputFile : "jsonReports/jsonReport.json"} ], ['html', {open: 'never'}]]
+
+});
